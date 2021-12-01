499a7e375ed097dcf556456f03a16a967cacfa07
Lando de Weerdt <lando.deweerdt@gmail.com>
2021-12-01 12:06:17 +0100

    Moved files. Updated gitignore

--- yolor/webservice.py
+++ yolor/webservice.py
@@ -15,18 +15,19 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-from yolor.detect import detect
+from models.models import *
+from detect import detect
+
 from flask import Flask
 from flask import request
 from flask import render_template
-
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'C:\\Users\\Wight\\PycharmProjects\\BirdDetector\\WindTurbineBirdDetector\\DLWebService\\static'
 
 MODEL = None
-MODEL_CFG = '../yolor/cfg/yolor_p6.cfg'
-MODEL_WEIGHTS = '../yolor/trained_weigths/my_model.pt'
+MODEL_CFG = 'cfg/yolor_p6.cfg'
+MODEL_WEIGHTS = 'trained_weigths/my_model.pt'
 MODEL_IMG_SIZE = 1280
 DEVICE = 'cuda'
 
@@ -47,12 +48,12 @@ def upload_predict():
 
 if __name__ == "__main__":
     # Load model
-    # model = Darknet(MODEL_CFG, MODEL_IMG_SIZE).cuda()
-    # model.load_state_dict(torch.load(MODEL_WEIGHTS, map_location=DEVICE)['model'])
-    # #model = attempt_load(weights, map_location=device)  # load FP32 model
-    # #imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
-    # model.to(DEVICE).eval()
-    # model.half()  # to FP16
-    detect()
+    model = Darknet(MODEL_CFG, MODEL_IMG_SIZE).cuda()
+    model.load_state_dict(torch.load(MODEL_WEIGHTS, map_location=DEVICE)['model'])
+    #model = attempt_load(weights, map_location=device)  # load FP32 model
+    #imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    model.to(DEVICE).eval()
+    model.half()  # to FP16
+    # detect()
     print('done')
     app.run(port=12000, debug=True)
